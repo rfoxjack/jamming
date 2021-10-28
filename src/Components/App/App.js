a1873358d4161619a986fc426483b9169cd40854
rfoxjack <randie.foxjackson@gmail.com>
2021-10-27 20:02:07 -0400

    static components created

---
+++ src/Components/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* Add a searchbar component */};
+          <div className="App-playlist">
+            {/* Add a searchresults component */}
+            {/* Add a playlist component */ }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
