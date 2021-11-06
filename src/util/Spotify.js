26af4d4c97893b4443b7e12e616ac7abad3ace70
rfoxjack <randie.foxjackson@gmail.com>
2021-11-06 19:02:31 -0400

    Spotify access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+const clientId = 'cf5a5191080a40c188b0a0f4d9196ceb';    // Remove before adding to github
+const redirectUri = 'http://localhost:3000';
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // Check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            // This clears the parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    }
+}
+
+export default Spotify;
